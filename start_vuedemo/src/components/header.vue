8129a61d479f489d2b4576c47fa33522e585ff4a
Zoezcc <438275452@qq.com>
2018-01-28 19:37:44 +0800

    vue demo without vue-cli

---
+++ start_vuedemo/src/components/header.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <div>
+        <h1>共同header</h1>
+        <img src="../assets/images/logo.png">
+    </div>
+</template>
+<style>
+    @import '../assets/sass/reset.scss';
+</style>
